3b821f95bf06966879cc1cebce3416f820cf16d1
Benjamin Carleski <benjamin@proworks.com>
2020-02-11 17:48:42 -0800

    Add ImageSharp implementation

---
+++ src/Umbraco.Infrastructure.ImageSharp/ImageSharpComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Infrastructure.ImageSharp
+{
+    public class ImageSharpComposer : ICoreComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.RegisterUnique<IImageUrlGenerator, ImageSharpImageUrlGenerator>();
+        }
+    }
+}
